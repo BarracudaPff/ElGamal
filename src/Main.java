3ddd909d1b6d861e792d6e5dd8f957a9e491854b
BarracudaPff <mrm.kikll@gmail.com>
2019-05-03 00:17:15 +0300

    add documentation

--- src/Main.java
+++ src/Main.java
@@ -5,14 +5,12 @@ import java.util.*;
 
 public class Main {
 
-    private static int max = 200;
-
     public static void main(String[] args) {
-        new Main().exec2();
+        new Main().exec();
     }
 
-    void exec2() {
-        BigInteger p = randSimple();
+    void exec() {
+        BigInteger p = randPrime();
 
         Scanner scanner = new Scanner(System.in);
         System.out.println("Enter from 0 to " + p);
@@ -23,31 +21,23 @@ public class Main {
         BigInteger x = rand(p);
 
         BigInteger y = pow(g, x).mod(p);
-        System.out.println("Public key = " + y);
+
+        Pair<BigInteger, BigInteger> pair = encrypt(m, g, y, p);
+
+        System.out.println("private key = " + y);
         System.out.println("Private key = " + x);
-        Pair<BigInteger, BigInteger> pair = encrypte(m, g, y, p);
         System.out.println("Pair " + pair);
-        System.out.println(decrypte(pair, x, p));
+        System.out.println("Message is " + m);
+        BigInteger decr = decrypt(pair, x, p);
+        System.out.println("Message decrypted as " + decr);
     }
 
-    void exec() {
-        /*BigInteger m = new BigInteger("10014");
-        BigInteger p = randSimple();
-        BigInteger g = rand();
-
-        BigInteger x = rand();//Private key
-        BigInteger y = pow(g, x);//Public key
-
-        Pair<BigInteger, BigInteger> pair = encrypte(m, g, y);//encr
-        BigInteger decrypte = decrypte(pair, x);//decr
-
-        System.out.println(g + "\t" + x + "\t" + y);
-        System.out.println(pair);
-        System.out.println("Was = " + m + "; Become = " + decrypte + ".");*/
-
-    }
-
-    private BigInteger randSimple() {
+    /**
+     * Get random prime BigInteger
+     *
+     * @return random prime BigInteger
+     */
+    private BigInteger randPrime() {
         BigInteger number = rand();
         while (!number.isProbablePrime(100)) {
             number = rand();
@@ -55,23 +45,39 @@ public class Main {
         return number;
     }
 
-    private BigInteger decrypte(Pair<BigInteger, BigInteger> pair, BigInteger x, BigInteger p) {
+    /**
+     * Decrypt message
+     *
+     * @param pair pair of keys (a,b)
+     * @param x    public key
+     * @param p    module
+     * @return decrypted message
+     */
+    private BigInteger decrypt(Pair<BigInteger, BigInteger> pair, BigInteger x, BigInteger p) {
         return (pair.getValue()
-                .multiply(getSmth(pow(pair.getKey(), x), p)))
+                .multiply(getMod(pow(pair.getKey(), x), p)))
                 .mod(p);
-        //.divide(pow(pair.getKey(), x));
     }
 
     /**
-     * @return
+     * Get random from 1 to max (200)
+     *
+     * @return rand from 1 to max
      */
     private BigInteger rand() {
         Random r = new Random();
         int low = 1;
+        int max = 200;
         int high = max;
         return new BigInteger(String.valueOf(r.nextInt(high - low) + low));
     }
 
+    /**
+     * Get BigInteger random number
+     *
+     * @param n max border
+     * @return random from (0,p-1)
+     */
     private BigInteger rand(BigInteger n) {
         Random rand = new Random();
         BigInteger result = new BigInteger(n.bitLength(), rand);
@@ -85,7 +91,16 @@ public class Main {
         return result;
     }
 
-    private Pair<BigInteger, BigInteger> encrypte(BigInteger m, BigInteger g, BigInteger y, BigInteger p) {
+    /**
+     * Encrypt message
+     *
+     * @param m message to encrypt
+     * @param g random number
+     * @param y private key
+     * @param p module
+     * @return encrypted message
+     */
+    private Pair<BigInteger, BigInteger> encrypt(BigInteger m, BigInteger g, BigInteger y, BigInteger p) {
         BigInteger r = rand(p);
         return new Pair<>(
                 pow(g, r).mod(p),
@@ -93,7 +108,14 @@ public class Main {
         );
     }
 
-    public static BigInteger pow(BigInteger base, BigInteger exponent) {
+    /**
+     * finds pow from BigIntegers
+     *
+     * @param base     power from
+     * @param exponent power to
+     * @return base^exponent
+     */
+    private static BigInteger pow(BigInteger base, BigInteger exponent) {
         BigInteger result = BigInteger.ONE;
         while (exponent.signum() > 0) {
             if (exponent.testBit(0)) result = result.multiply(base);
@@ -103,23 +125,26 @@ public class Main {
         return result;
     }
 
-    //generate random num
-    BigInteger genKey(BigInteger q) {
-        BigInteger key = rand();
-        while (!Objects.equals(q.gcd(q), BigInteger.ONE)) {
-            key = q.add(rand().mod(new BigInteger("3646")));
-        }
-        return key;
-    }
-
-    public static BigInteger getPRoot(BigInteger p) {
+    /**
+     * Finds P Root for p
+     *
+     * @param p number for witch P Root should be found
+     * @return P Root for p
+     */
+    private static BigInteger getPRoot(BigInteger p) {
         for (BigInteger i = BigInteger.ZERO; i.compareTo(p) < 0; i = i.add(BigInteger.ONE))
-            if (IsPRoot(p, i))
+            if (isPRoot(p, i))
                 return i;
         return BigInteger.ZERO;
     }
 
-    public static boolean IsPRoot(BigInteger p, BigInteger a) {
+    /**
+     * Check if a is P Root ( p mod a == 1)
+     *
+     * @param a module
+     * @return is p mod a == 1
+     */
+    private static boolean isPRoot(BigInteger p, BigInteger a) {
         if (a.equals(BigInteger.ZERO) || a.equals(BigInteger.ONE))
             return false;
         BigInteger last = BigInteger.ONE;
@@ -134,7 +159,15 @@ public class Main {
         return true;
     }
 
-    public BigInteger getSmth(BigInteger a, BigInteger m) {
+    /**
+     * find b from a * b == m
+     * b goes from 1 to INFINITY
+     *
+     * @param a one of multipliers
+     * @param m module
+     * @return another multipliers
+     */
+    private BigInteger getMod(BigInteger a, BigInteger m) {
         BigInteger b = BigInteger.ONE;
         while (!a.multiply(b).mod(m).equals(BigInteger.ONE)) {
             b = b.add(BigInteger.ONE);
